4de7fe1bfc9c63bbdfc8c6c855c02bd36541d9a1
Subhiraj Singh <subhiraj.singh@cpaperless.com>
2021-07-11 21:37:19 +0530

    Add project files.

---
+++ ChatApp/ChatHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Dynamic;
+using System.Threading.Tasks;
+using WebSocketManager;
+
+namespace ChatApp
+{
+    public class ChatHandler : WebSocketHandler
+    {
+        private readonly ChatManager _chatManager;
+        public ChatHandler(WebSocketConnectionManager webSocketConnectionManager, ChatManager chatManager) : base(webSocketConnectionManager)
+        {
+            _chatManager = chatManager;
+        }
+        public async Task SendMessage(string socketId, string message)
+        {
+            dynamic dynamicMessage = new ExpandoObject();
+            dynamicMessage.UserId = socketId;
+            dynamicMessage.Message = message;
+            _chatManager.Messages.Add(dynamicMessage);
+            await InvokeClientMethodToAllAsync("pingMessage", socketId, message).ConfigureAwait(false);
+        }
+    }
+}
